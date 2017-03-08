e22922a1fb298597b886862588815a8a2fb5fb61
eros.chen <eros.chen@fuhu.com>
2017-03-08 19:02:05 +0800

    add LocationManager

--- EatWhat/ViewController.swift
+++ EatWhat/ViewController.swift
@@ -7,10 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    
+    @IBOutlet weak var sliderBar: UISlider!
+    @IBOutlet weak var distanceLable: UILabel!
+    @IBOutlet  var map: MKMapView!
+    let locationManager = CLLocationManager()
+    @IBAction func searchClickListener(_ sender: Any) {
+        
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestLocation()
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +38,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print("didUpdateLocations")
+        
+        let location = locations.first!
+        print("\(location.coordinate)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print("didFailWithError")
+        
+    }
 
 }
 
