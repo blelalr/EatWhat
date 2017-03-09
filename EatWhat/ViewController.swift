69e1f309dc0313c4bd9d41aab44df08345ff49ca
eros.chen <blelalrg@gmail.com>
2017-03-09 17:44:40 +0800

    add slider

--- EatWhat/ViewController.swift
+++ EatWhat/ViewController.swift
@@ -16,20 +16,39 @@ class ViewController: UIViewController, LocationUpdateProtocol {
     @IBOutlet weak var distanceLable: UILabel!
     @IBOutlet  var map: MKMapView!
     
-    var currentLocation : CLLocation!
+    let distance = 0// 0.3~2.0 km default 0.5km
+    var currentLocation : CLLocation?
     
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        distanceLable.text = String(format: "%.1f", sender.value)
+        
+    }
     @IBAction func searchClickListener(_ sender: Any) {
 
         let locationMgr = LocationManager.SharedManager
         locationMgr.delegate = self
         locationMgr.requestLocation()
+        
     }
     
-    //LocationUpdateProtocol 
+    //LocationUpdateProtocol
     func locationDidUpdateToLocation(location: CLLocation) {
         currentLocation = location
-        print("Latitude : \(self.currentLocation.coordinate.latitude)")
-        print("Longitude : \(self.currentLocation.coordinate.longitude)")
+        let latitude  = currentLocation?.coordinate.latitude
+        let longitude = currentLocation?.coordinate.longitude
+        
+        print("******* Current Location *******")
+        print("Latitude : \(latitude)")
+        print("Longitude : \(longitude)")
+        print("*********************************")
+        
+//        setUI()
+    }
+    
+    func setUI(){
+    
+        
+        
     }
 
 }
