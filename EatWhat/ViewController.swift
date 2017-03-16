2048bdaef6f364bdc5cffc03e8b130fd126e477e
eros.chen <eros.chen@fuhu.com>
2017-03-16 18:11:29 +0800

    1.call api to get data 2.use stack VIEW

--- EatWhat/ViewController.swift
+++ EatWhat/ViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 
 class ViewController: UIViewController{
 
     @IBOutlet weak var sliderBar: UISlider!
     @IBOutlet weak var distanceLable: UILabel!
+    @IBOutlet weak var storeLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var resultDistanceLabel: UILabel!
+    @IBOutlet weak var resultTimeLabel: UILabel!
+    
     @IBOutlet  var map: MKMapView!
     
     let distance = 0// 0.3~2.0 km default 0.5km
-    var currentLocation : CLLocation?
     
     let locationManager = LocationManager()
     
@@ -27,10 +30,72 @@ class ViewController: UIViewController{
     }
     @IBAction func searchClickListener(_ sender: Any) {
 //                     .requestLocation(completionHandler: { currentLocation in }
-        locationManager.requestLocation { (currentLocation) in
-            print("from ViewController: \(currentLocation)")
+        locationManager.requestLocation { (location) in
+            print("from ViewController: \(location)")
+            self.apiGetRestaurant(location: location)
         }
         
+        
+        
+    }
+    
+    func apiGetRestaurant(location:CLLocation){
+        let distance = self.sliderBar.value
+        let session = URLSession.shared
+        
+        let url = URL(string: "https://food-locator-dot-hpd-io.appspot.com/v1/location_query?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&distance=\(distance)")!
+        
+        print("\(url)")
+        
+        let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+            if let error = error {
+                print("API下載錯誤: \(error)")
+                return
+            }
+            
+            let data = data!
+            
+            if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers), let results = jsonObject as? [[String: Any]], let firstResult = results.first {
+                
+                let currentLocationPlacemark = MKPlacemark(coordinate: location.coordinate, addressDictionary: nil)
+                let currentLocationMapItem = MKMapItem(placemark: currentLocationPlacemark)
+                
+                let latitude = firstResult["latitude"] as! Double
+                let longitude = firstResult["longitude"] as! Double
+                
+                let destionationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                let destinationPlacemark = MKPlacemark(coordinate: destionationCoordinate, addressDictionary: nil)
+                let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+                
+                let request = MKDirectionsRequest()
+                request.source = currentLocationMapItem
+                request.destination = destinationMapItem
+                request.transportType = .walking
+                
+                let directions = MKDirections(request: request)
+                directions.calculateETA(completionHandler: { response, error in
+                    
+                    if let error = error {
+                        print("路徑規劃錯誤: \(error)")
+                        return
+                    }
+                    
+                    let response = response!
+                    print("結果: \(response.expectedTravelTime / 60.0), \(response.distance)")
+                    
+                    print("\(firstResult)")
+                    
+                })
+                
+                
+                //                    self.storeNameLabel.text = firstResult["name"] as! String
+                //                    self.ratingLabel.text = "\(firstResult["rating"] as! Double)"
+                //                    self.addressLabel.text = firstResult["address"] as! String
+            }
+            
+        })
+        
+        task.resume()
     }
     
 }
