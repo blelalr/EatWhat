cfa61bab5c309c3a310b688315ee2e1cebad50c7
Eros <blelarg@gmail.com>
2017-03-19 18:08:48 +0800

    use GCD to start Queue

--- EatWhat/ViewController.swift
+++ EatWhat/ViewController.swift
@@ -18,101 +18,128 @@ class ViewController: UIViewController{
     @IBOutlet weak var resultDistanceLabel: UILabel!
     @IBOutlet weak var resultTimeLabel: UILabel!
     
+    @IBOutlet weak var address: UILabel!
     @IBOutlet weak var map: MKMapView!
     
     let distance = 0// 0.3~2.0 km default 0.5km
-    
     let locationManager = LocationManager()
+    var phone: String?
+    
+    let queue = DispatchQueue.global(qos: .background)
+    var time:DispatchTime! {
+        return DispatchTime.now() + 1.0 // seconds
+    }
     
     @IBAction func sliderValueChange(_ sender: UISlider) {
         distanceLable.text = String(format: "%.1f", sender.value)
+        
+    }
+    
+    @IBAction func callStore(_ sender: Any) {
+        
+        guard let number = URL(string: "telprompt://" + phone!) else { return }
+        UIApplication.shared.open(number, options: [:], completionHandler: nil)
         
     }
     @IBAction func searchClickListener(_ sender: Any) {
 //                     .requestLocation(completionHandler: { currentLocation in }
         locationManager.requestLocation { (location) in
             print("from ViewController: \(location)")
-            self.apiGetRestaurant(location: location)
+            self.startTask(curLocation: location)
         }
-        
-        
-        
     }
     
-    func apiGetRestaurant(location:CLLocation){
+    func startTask(curLocation: CLLocation){
         let distance = self.sliderBar.value
         let session = URLSession.shared
         
-        let url = URL(string: "https://food-locator-dot-hpd-io.appspot.com/v1/location_query?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&distance=\(distance)")!
+        let url = URL(string: "https://food-locator-dot-hpd-io.appspot.com/v1/location_query?latitude=\(curLocation.coordinate.latitude)&longitude=\(curLocation.coordinate.longitude)&distance=\(distance)")!
         
         print("\(url)")
         
-        let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+        queue.async(execute: {
+            let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let error = error {
+                    print("API下載錯誤: \(error)")
+                    return
+                }
+                
+                let data = data!
+                
+                if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers), let results = jsonObject as? [[String: Any]] {
+                    let randomIndex = Int(arc4random_uniform(UInt32(results.count)))
+                    print("\(results[randomIndex])")
+                    DispatchQueue.main.asyncAfter(deadline: self.time, execute:{
+                        DispatchQueue.main.async {
+                            self.setData(result: results[randomIndex])
+                        }
+                        OperationQueue().addOperation {
+                            self.drawMap(result: results[randomIndex], curLocation: curLocation)
+                        }
+                    })
+                }
+                
+            })
+            task.resume()
+        })
+        
+    }
+    
+    func setData(result:[String: Any]){
+        self.storeLabel.text = (result["name"] as! String)
+        self.rateLabel.text = "\(result["rating"] as! Double)"
+        self.address.text = (result["address"] as! String)
+        self.phone = (result["phone"] as! String)
+    }
+    
+    func drawMap(result: [String : Any], curLocation: CLLocation){
+        let currentLocationPlacemark = MKPlacemark(coordinate: curLocation.coordinate, addressDictionary: nil)
+        let currentLocationMapItem = MKMapItem(placemark: currentLocationPlacemark)
+        
+        let latitude = result["latitude"] as! Double
+        let longitude = result["longitude"] as! Double
+        
+        let destionationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let destinationPlacemark = MKPlacemark(coordinate: destionationCoordinate, addressDictionary: nil)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let request = MKDirectionsRequest()
+        request.source = currentLocationMapItem
+        request.destination = destinationMapItem
+        request.transportType = .walking
+        
+//        let directions = MKDirections(request: request)
+//        directions.calculate(completionHandler: { (response : MKDirectionsResponse?, error : Error?) in
+//            response?.routes.first?.polyline
+//        })
+        
+        
+        let directions = MKDirections(request: request)
+        directions.calculateETA(completionHandler: { response, error in
+            
             if let error = error {
-                print("API下載錯誤: \(error)")
+                print("路徑規劃錯誤: \(error)")
                 return
             }
             
-            let data = data!
+            let response = response!
+            print("結果: \(response.expectedTravelTime / 60.0), \(response.distance)")
             
-            if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers), let results = jsonObject as? [[String: Any]], let firstResult = results.first {
-                
-                let currentLocationPlacemark = MKPlacemark(coordinate: location.coordinate, addressDictionary: nil)
-                let currentLocationMapItem = MKMapItem(placemark: currentLocationPlacemark)
-                
-                let latitude = firstResult["latitude"] as! Double
-                let longitude = firstResult["longitude"] as! Double
-                
-                let destionationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                let destinationPlacemark = MKPlacemark(coordinate: destionationCoordinate, addressDictionary: nil)
-                let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
-                
-                let request = MKDirectionsRequest()
-                request.source = currentLocationMapItem
-                request.destination = destinationMapItem
-                request.transportType = .walking
-                
-                let directions = MKDirections(request: request)
-                directions.calculateETA(completionHandler: { response, error in
-                    
-                    if let error = error {
-                        print("路徑規劃錯誤: \(error)")
-                        return
-                    }
-                    
-                    let response = response!
-                    print("結果: \(response.expectedTravelTime / 60.0), \(response.distance)")
-                    
-                    print("\(firstResult)")
-                    
-                    let pointAnnotation  = MKPointAnnotation()
-                    pointAnnotation.coordinate = destionationCoordinate
-                    self.map.removeAnnotations(self.map.annotations)
-                    self.map.addAnnotation(pointAnnotation)
-                    self.map.showsUserLocation = true
-                    
-                    let degree = 1/111 * 0.5
-                    var mapRegion = MKCoordinateRegion()
-                    mapRegion.center = destionationCoordinate
-                    mapRegion.span.latitudeDelta = degree
-                    mapRegion.span.longitudeDelta = degree
-                    
-                    self.map.setRegion(mapRegion, animated: true)
-                    
-                
-                    
-                    
-                })
-                
-                
-//                self.storeNameLabel.text = firstResult["name"] as! String
-//                self.ratingLabel.text = "\(firstResult["rating"] as! Double)"
-//                self.addressLabel.text = firstResult["address"] as! String
-            }
+            let pointAnnotation  = MKPointAnnotation()
+            pointAnnotation.coordinate = destionationCoordinate
+            self.map.removeAnnotations(self.map.annotations)
+            self.map.addAnnotation(pointAnnotation)
+            self.map.showsUserLocation = true
+            
+            let degree = 1/111 * 0.5
+            var mapRegion = MKCoordinateRegion()
+            mapRegion.center = destionationCoordinate
+            mapRegion.span.latitudeDelta = degree
+            mapRegion.span.longitudeDelta = degree
+            
+            self.map.setRegion(mapRegion, animated: true)
             
         })
-        
-        task.resume()
     }
     
 }
